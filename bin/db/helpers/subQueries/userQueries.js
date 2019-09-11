362009bfb4327b625a35ced32acf57282c8278f0
Selin Huang <selin.hyx@gmail.com>
2019-09-11 00:48:58 -0400

    delete friend feature done

--- bin/db/helpers/subQueries/userQueries.js
+++ bin/db/helpers/subQueries/userQueries.js
@@ -1,77 +1,77 @@
-const db = require('../../../../db/connection/db');
+const db = require("../../../../db/connection/db");
 
 const getUserInfo = user_id => {
-	return db
-		.query({
-			text: `SELECT email, username, avatar, status
+  return db
+    .query({
+      text: `SELECT email, username, avatar, status
 			FROM users
 			where id = $1
 			;`,
-			values: [user_id],
-			name: 'get_user_info'
-		})
-		.then(res => res.rows[0]);
+      values: [user_id],
+      name: "get_user_info"
+    })
+    .then(res => res.rows[0]);
 };
 
 const getNewFriendInfo = email => {
-	return db
-		.query({
-			text: 'SELECT username, email, avatar, status, id FROM users WHERE email=$1 AND is_active=true;',
-			values: [email],
-			name: 'get_new_friend_info'
-		})
-		.then(res => res.rows[0]);
+  return db
+    .query({
+      text: "SELECT username, email, avatar, status, id FROM users WHERE email=$1 AND is_active=true;",
+      values: [email],
+      name: "get_new_friend_info"
+    })
+    .then(res => res.rows[0]);
 };
 
 //GET MY FRIENDLIST FUNCTION
 //removed invitiation_accepted_ not null
 const getFriendInfo = user_id => {
-	return db
-		.query({
-			text: `SELECT f.invitation_accepted_at, f.friend_id as id, u.email as email, u.username as username, u.avatar as avatar, u.status as status
+  return db
+    .query({
+      text: `SELECT f.invitation_accepted_at, f.friend_id as id, u.email as email, u.username as username, u.avatar as avatar, u.status as status
 			FROM friendlists fl join friends f on fl.id = f.friendlist_id
 			join users u on u.id = f.friend_id
 			where fl.user_id = $1 and u.is_active = true
 			;`,
-			values: [user_id],
-			name: 'get_friend_list'
-		})
-		.then(res => res.rows);
+      values: [user_id],
+      name: "get_friend_list"
+    })
+    .then(res => res.rows);
 };
 
 const deleteFriend = (user_id, friend_id) => {
-	return db
-		.query({
-			text: `
-			DELETE FROM friends WHERE id IN
-			(SELECT f.id
-			FROM users u join friendlists fl on u.id = fl.user_id
-			join friends f on f.friendlist_id = fl.id
-			WHERE u.id = $1 and f.friend_id = $2)
-			RETURNING *;`,
-			values: [user_id, friend_id],
-			name: 'delete_friend'
-		})
-		.then(() => getFriendInfo(user_id));
+  console.log("info being passed to the deleteFriend query:", user_id, friend_id);
+  return db
+    .query({
+      text: `DELETE FROM friends WHERE id IN (SELECT f.id
+				FROM users u join friendlists fl on u.id = fl.user_id
+				join friends f on f.friendlist_id = fl.id
+				WHERE (u.id = $1 and f.friend_id = $2) or (u.id = $2 and f.friend_id = $1))
+				RETURNING *;`,
+
+      values: [user_id, friend_id],
+      name: "delete_friend"
+    })
+    .then(res => getFriendInfo(user_id));
 };
 
 const addFriend = (user_id, friend_id) => {
-	return db
-		.query({
-			text: `INSERT INTO friends (friend_id, friendlist_id)
+  return db
+    .query({
+      text: `INSERT INTO friends (friend_id, friendlist_id)
 		SELECT $2,
 		id FROM friendlists
 		WHERE user_id = $1 RETURNING *;`,
-			values: [user_id, friend_id],
-			name: 'add_friend'
-		})
-		.then(() => getFriendInfo(user_id));
+      values: [user_id, friend_id],
+      name: "add_friend"
+    })
+    .then(() => getFriendInfo(user_id));
 };
 
 module.exports = {
-	getUserInfo,
-	getFriendInfo,
-	deleteFriend,
-	addFriend,
-	getNewFriendInfo
+  getUserInfo,
+  getFriendInfo,
+  deleteFriend,
+  addFriend,
+  getNewFriendInfo
 };
